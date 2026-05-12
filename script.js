204490b30d27dff6ea47b377129a1c94ff107885
whaychuin <whay.chuin@gmail.com>
2026-05-12 16:51:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbwLv3iFXP2oyuYPVMVo7Y6LqVs3kjnQjV9He4kEiWJAgXAru2XkeLdGK2l3FZnn2mY79w/exec";
 
+fetch(scriptURL)
+  .then(response => response.json())
+  .then(data => {
+
+    const lecturerDropdown = document.getElementById("pensyarah");
+    const practicumDropdown = document.getElementById("praktikum");
+
+    data.lecturers.forEach(name => {
+      const option = document.createElement("option");
+      option.value = name;
+      option.textContent = name;
+      lecturerDropdown.appendChild(option);
+    });
+
+    data.practicums.forEach(practicum => {
+      const option = document.createElement("option");
+      option.value = practicum;
+      option.textContent = practicum;
+      practicumDropdown.appendChild(option);
+    });
+
+  })
+  .catch(error => {
+    console.error("Dropdown loading error:", error);
+  });
+
 document.getElementById("attendanceForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
